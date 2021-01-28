7ff7d295a0c6c57627d6510ea03583c898b05fde
sujith <sujithallala.1993@gmail.com>
2021-01-27 21:19:47 -0500

    added user and lead  models

---
+++ leads/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+class Lead(models.Model):
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
+    
+
+    
+
+    
